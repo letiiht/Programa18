04ce11194c79450835d28aa6e7e106795db897b1
lety <107319962+letiiht@users.noreply.github.com>
2022-10-02 14:44:04 -0500

    elimine problema

--- act18.cpp
+++ act18.cpp
@@ -1,14 +1,3 @@
-/*Una empresa quiere hacer una compra de varias piezas de la misma clase a una fábrica de
-refacciones. La empresa, dependiendo del monto total de la compra, decidirá qué hacer para
-pagar al fabricante.
-a) Si el monto total de la compra excede de $500 000 la empresa tendrá la capacidad de
-invertir de su propio dinero un 55% del monto de la compra, pedir prestado al banco un 30%
-y el resto lo pagará solicitando un crédito al fabricante.
-b) Si el monto total de la compra no excede de $500 000 la empresa tendrá capacidad de
-invertir de su propio dinero un 70% y el restante 30% lo pagará solicitando crédito al
-fabricante.
-c) El fabricante cobra por concepto de intereses un 20% sobre la cantidad que se le pague a
-crédito.*/
 #include<iostream>
 #include<conio.h>
 using namespace std;
